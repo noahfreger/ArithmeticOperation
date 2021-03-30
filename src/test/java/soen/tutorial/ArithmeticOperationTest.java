c02ad68f0bed673684a9676af3b889339dbdc4f6
noahfreger <50425626+noahfreger@users.noreply.github.com>
2021-03-30 12:24:42 -0400

    Update ArithmeticOperationTest.java

--- src/test/java/soen/tutorial/ArithmeticOperationTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationTest.java
@@ -15,19 +15,19 @@ public class ArithmeticOperationTest {
 		ArithmeticOperation operations = new ArithmeticOperation();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		assertEquals(expected, actual);
-		//fail("must fail");
+		//assertEquals(expected, actual);
+		fail("must fail");
 
 	}
-    @Test
-    public void testSub()
-    {
+//     @Test
+//     public void testSub()
+//     {
 
-        ArithmeticOperation operations = new ArithmeticOperation();
-        Integer actual = operations.addOrSub(6, 2);
-        Integer expected = 4;
-        assertEquals(expected, actual);
+//         ArithmeticOperation operations = new ArithmeticOperation();
+//         Integer actual = operations.addOrSub(6, 2);
+//         Integer expected = 4;
+//         assertEquals(expected, actual);
 
-    }
+//     }
 
 }
